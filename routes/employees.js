146100aa28c51c5f19f8ec9d314dc72b99367790
root <root@localhost.localdomain>
2019-04-24 01:54:49 +0300

    Added more validation on GET parameters

--- routes/employees.js
+++ routes/employees.js
@@ -13,12 +13,26 @@ router.get("/", (req, res, next) => {
 	});
 });
 
-router.get("/limit/:limit([0-9]+)", (req, res, next) => {
-	employees.GetAllEmployees(req.params.limit, (err, result) => {
-	  res.setHeader("Content-Type", "application/json");
-	  res.send(result);
-  	});
-});
+router.get("/limit/:limit", [
+		check("limit")
+		.isInt({
+			gt : 0
+		})
+		.withMessage("Limit must be a positive integer")
+	], (req, res, next) => {
+		res.setHeader("Content-Type", "application/json");
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.send({
+				errors : errors.array()
+			});
+			return;
+		}
+		employees.GetAllEmployees(req.params.limit, (err, result) => {
+			res.setHeader("Content-Type", "application/json");
+			res.send(result);
+		});
+	});
 
 router.get("/firstname/:fname", [
 		check("fname").isAlpha()
@@ -38,26 +52,28 @@ router.get("/firstname/:fname", [
 	});
 
 router.get("/firstname/:fname/limit/:limit",[
-			check("fname")
-			.isAlpha()
-			.withMessage("Firstname must only contain alphabetic characters"),
-			check("limit")
-			.isInt()
-			.withMessage("Limit must be an integer")
-		], (req, res, next) => {
-			res.setHeader("Content-Type", "application/json");
-			const errors = validationResult(req);
-			if (!errors.isEmpty()) {
-				res.send({
-					errors : errors.array()
-				});
-				return;
-			}
-
-			employees.GetEmployeesByFirstName(req.params.fname, req.params.limit, (err, result) => {
-				res.send(result);
+		check("fname")
+		.isAlpha()
+		.withMessage("Firstname must only contain alphabetic characters"),
+		check("limit")
+		.isInt({
+			gt : 0
+		})
+		.withMessage("Limit must be a positive integer")
+	], (req, res, next) => {
+		res.setHeader("Content-Type", "application/json");
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.send({
+				errors : errors.array()
 			});
+			return;
+		}
+
+		employees.GetEmployeesByFirstName(req.params.fname, req.params.limit, (err, result) => {
+			res.send(result);
 		});
+	});
 
 router.post("/", [
 		check("firstname").isAlpha()
