90e9b2b552632c650f44666f4069dd1078d0a0d3
root <root@localhost.localdomain>
2019-04-25 01:03:34 +0300

    Added custom error 404 message

--- app.js
+++ app.js
@@ -21,21 +21,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/employees', employeesRouter);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
+app.use("*", (req, res, next) => {
+	res.status(404).send("<h1>Error 404: Page not found</h1>");
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
