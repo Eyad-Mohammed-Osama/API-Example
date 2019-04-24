df46553434b60dd3089d5b83174df437718b325b
root <root@localhost.localdomain>
2019-04-25 02:04:19 +0300

    Added express-session middleware

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
 
-const indexRouter = require('./routes/index');
-const employeesRouter = require('./routes/employees');
+const indexRouter = require("./routes/index");
+const employeesRouter = require("./routes/employees");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session({ secret : "This is a secret" }));
 
-app.use('/', indexRouter);
-app.use('/employees', employeesRouter);
+app.use("/", indexRouter);
+app.use("/employees", employeesRouter);
 app.use("*", (req, res, next) => {
 	res.status(404).send("<h1>Error 404: Page not found</h1>");
 });
@@ -29,11 +31,11 @@ app.use("*", (req, res, next) => {
 app.use((err, req, res, next) => {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
 	// render the error page
 	res.status(err.status || 500);
-	res.render('error');
+	res.render("error");
 });
 
 module.exports = app;
