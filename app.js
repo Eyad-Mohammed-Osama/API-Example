dcb87e47aa230cf1995ed6e70f7f6a0822d5a13d
Eyad-Syrialover <eyadbere@gmail.com>
2019-04-28 02:07:12 +0300

    Enhancements for error view and rendering

--- app.js
+++ app.js
@@ -35,11 +35,12 @@ app.use((err, req, res, next) => {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get("env") === "development" ? err : {};
-	console.log(err.stack)
+	console.log(JSON.stringify(err))
 	// render the error page
 	res.status(err.status || 500);
-	res.render("error", {
-		message : err.message,
+	res.render("layout", {
+		title : "An error has occured",
+		view : "error",
 		error : err
 	});
 });
