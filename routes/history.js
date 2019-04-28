9326d7f3cfa7bd3b7d1512e2ee6e7e9968e95059
Eyad-Syrialover <eyadbere@gmail.com>
2019-04-29 00:21:47 +0300

    Removed unnecessary log

--- routes/history.js
+++ routes/history.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    console.log(req.session.queries)
     res.render('layout', {
 		title : "Search history",
         view : "search-history",
