bf1207cf7ad0b121d41338dd8e1c478c5bdc520a
Eyad-Syrialover <eyadbere@gmail.com>
2019-04-29 00:15:56 +0300

    Moved database configuration to a seperate file

---
+++ models/db-credentials.js
@@ -0,0 +1,10 @@
+// Global database configuration
+const DBCredentials = {
+    host : "localhost",
+    port : 3306,
+    user : "root",
+    password : "",
+    database : "company",
+};
+
+module.exports = DBCredentials;
